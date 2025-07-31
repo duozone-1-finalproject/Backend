8551479a79e55e621cb6f2459b96eec346132491
kyusangshim <ks030788@naver.com>
2025-07-31 10:16:53 +0900

    chore: #3 model > User.java elasticsearch에 맞게 변경

--- src/main/java/com/example/finalproject/login_auth/model/User.java
+++ src/main/java/com/example/finalproject/login_auth/model/User.java
@@ -2,45 +2,30 @@ package com.example.finalproject.login_auth.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
 
-
-@Entity
-@Table(name = "users")
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Document(indexName = "users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
 
-    // 로컬 회원가입만 사용
-    @Column(unique = true)
     private String username;
 
     private String password;
 
-    // OAuth 방식과 로컬 방식 사용
-    @Column(unique = true)
     private String email;
 
     private String name;
 
     private String role;
 
-    // 네이버,구글,로컬 구별용 칼럼으로 각 계정으로 로그인시 자동으로 변경됨.
     private String provider;
-
 }
