32e4409fc0e067c9ecfa202b1492b9e0887c850a
wanter9091 <dhqudwo2976@naver.com>
2025-08-06 02:33:30 +0900

    Refactor : 엘라스틱서치 테스트중

---
+++ src/main/java/com/example/finalproject/dart/dto/CompanyOverview/CompanyOverviewRequestDto.java
@@ -0,0 +1,11 @@
+package com.example.finalproject.dart.dto.CompanyOverview;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CompanyOverviewRequestDto {
+    private String corpCode;
+    private String corpName;
+}
--- src/main/java/com/example/finalproject/dart/repository/CompanyOverviewRepository.java
+++ src/main/java/com/example/finalproject/dart/repository/CompanyOverviewRepository.java
@@ -1,13 +1,6 @@
 package com.example.finalproject.dart.repository;
-
 import com.example.finalproject.dart.entity.CompanyOverview;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface CompanyOverviewRepository extends ElasticsearchRepository<CompanyOverview, String> {
-    Optional<CompanyOverview> findByCorpCode(String corpCode);
-    Optional<CompanyOverview> findByCorpName(String corpName);
-    List<CompanyOverview> findAll();
 }
