e39e64bfa414f5f7ff5a5170bb7e704468c49782
wanter9091 <dhqudwo2976@naver.com>
2025-08-19 16:55:57 +0900

    Chore : 직원 현황 #24

--- src/main/java/com/example/finalproject/apitest/repository/periodic/DartUnregisteredExecutiveCompensationRepository.java
+++ src/main/java/com/example/finalproject/apitest/repository/periodic/DartUnregisteredExecutiveCompensationRepository.java
@@ -1,4 +1,4 @@
 package com.example.finalproject.apitest.repository.periodic;
 // 미등기임원 보수현황
-public interface DartUnregisteredExecutiveCompensation {
+public interface DartUnregisteredExecutiveCompensationRepository {
 }
