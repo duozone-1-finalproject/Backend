4f5cb1a395717ce29f970b6365f23a8fec466b76
JuKoNuNu <127651508+JuKoNuNu@users.noreply.github.com>
2025-08-23 13:11:55 +0900

    API 규상병합 #24

---
+++ src/main/java/com/example/finalproject/dart_viewer/config/WebConfigV2.java
@@ -0,0 +1,17 @@
+package com.example.finalproject.dart_viewer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigV2 implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로
+                .allowedOrigins("http://localhost:3000") // 허용할 프론트 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
+}
