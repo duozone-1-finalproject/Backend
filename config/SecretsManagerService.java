78344a050daefd22dacd107f4546931756fda3a3
SeoheeLim <135564947+SeoheeLim@users.noreply.github.com>
2025-08-28 16:34:44 +0900

    fix: db 빌드문제 수정4(#52)

--- config/SecretsManagerService.java
+++ config/SecretsManagerService.java
@@ -1,17 +1,24 @@
 package com.example.Backend.config;
 
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 
+@Service
 public class SecretsManagerService {
 
     private final SecretsManagerClient client;
 
     public SecretsManagerService() {
+        // DefaultCredentialsProvider는 환경 변수, 자바 시스템 속성, 프로필 파일,
+        // ECS 컨테이너 자격 증명, EC2 인스턴스 프로필 자격 증명(EKS 서비스 계정 포함)에서
+        // 자격 증명을 자동으로 찾습니다.
         this.client = SecretsManagerClient.builder()
                 .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 
