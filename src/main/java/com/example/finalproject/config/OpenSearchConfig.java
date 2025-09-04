4a576ccb88d84778988d970052f274153a129952
SeoheeLim <135564947+SeoheeLim@users.noreply.github.com>
2025-09-04 15:42:39 +0900

    fix: 백엔드 dev동기화(#55)

--- src/main/java/com/example/finalproject/config/OpenSearchConfig.java
+++ src/main/java/com/example/finalproject/config/OpenSearchConfig.java
@@ -8,6 +8,7 @@ import org.opensearch.client.RestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class OpenSearchConfig {
 
