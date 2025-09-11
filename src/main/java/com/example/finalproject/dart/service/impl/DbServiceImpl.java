824ac1816e617eb90d0e2c666292c20dc26c6a35
wanter9091 <dhqudwo2976@naver.com>
2025-09-11 13:30:32 +0900

    chore : 검색기 수정중

--- src/main/java/com/example/finalproject/dart/service/impl/DbServiceImpl.java
+++ src/main/java/com/example/finalproject/dart/service/impl/DbServiceImpl.java
@@ -4,7 +4,9 @@ import com.example.finalproject.dart.dto.CompanyOverview.CompanyOverviewListRequ
 import com.example.finalproject.dart.dto.CompanyOverview.CompanyOverviewListResponseDto;
 import com.example.finalproject.dart.dto.CompanyOverview.CompanyOverviewResponseDto;
 import com.example.finalproject.dart.entity.CompanyOverview;
+import com.example.finalproject.dart.entity.CorpCode;
 import com.example.finalproject.dart.repository.CompanyOverviewRepository;
+import com.example.finalproject.dart.repository.CorpCodeRepository;
 import com.example.finalproject.dart.service.DbService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +23,7 @@ import java.util.stream.Collectors;
 @Slf4j
 public class DbServiceImpl implements DbService {
     private final CompanyOverviewRepository companyOverviewRepository;
+    private final CorpCodeRepository corpCodeRepository;
 
 
     // db에 기업코드 리스트 넣기
@@ -49,7 +52,6 @@ public class DbServiceImpl implements DbService {
 
     }
 
-
     // 테스트
     public List<CompanyOverview> test(String word){
         try {
@@ -59,18 +61,34 @@ public class DbServiceImpl implements DbService {
         }
     }
 
-
     // db에서 기업들의 정보(기업이름,기업코드) 가져오기
     @Override
     public CompanyOverviewListResponseDto getAllCompanyOverviews(){
         // 컴퍼니 정보 전부 조회하기
         // http://localhost:8080/api/companies
-        List<CompanyOverview> companyOverviewList = null;
-        try {
-            companyOverviewList = companyOverviewRepository.findAll();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        List<CorpCode> companyOverviewList = null;
+        companyOverviewList = corpCodeRepository.findAll();
+
+        List<CompanyOverviewResponseDto> responseDtoList = companyOverviewList.stream()
+                .map(company -> CompanyOverviewResponseDto.builder()
+                        .corpCode(company.getCorpCode())
+                        .corpName(company.getCorpName())
+                        .build())
+                .toList();
+
+        return CompanyOverviewListResponseDto.builder()
+                .companies(responseDtoList)
+                .build();
+
+    }
+
+    // db에서 기업이름으로 검색해서 최대 100개까지 가져오기
+    @Override
+    public CompanyOverviewListResponseDto get100CorpCode(String keyword){
+        // 컴퍼니 정보 전부 조회하기
+        // http://localhost:8080/api/companies
+        List<CorpCode> companyOverviewList = null;
+        companyOverviewList = corpCodeRepository.findTop100ByCorpNameContainingOrderByCorpNameAsc(keyword);
 
         List<CompanyOverviewResponseDto> responseDtoList = companyOverviewList.stream()
                 .map(company -> CompanyOverviewResponseDto.builder()
