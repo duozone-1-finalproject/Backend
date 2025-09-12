c5a21fc8de3a523b658d8c2bb557829229f24f78
wanter9091 <dhqudwo2976@naver.com>
2025-09-12 11:17:42 +0900

    chore : 권한 수락

--- src/main/java/com/example/finalproject/ai_backend/controller/VariableMappingController.java
+++ src/main/java/com/example/finalproject/ai_backend/controller/VariableMappingController.java
@@ -15,6 +15,7 @@ import java.util.concurrent.CompletableFuture;
 @RestController
 @RequestMapping("/api/v1/variables")
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
 public class VariableMappingController {
 
     private final MainVariableMappingService mainVariableMappingService;
